db69dafae2da9fe8d42fbdad4fb981a67f7d246e
michel <michel.dolstra@bettyblocks.com>
2025-11-11 12:01:42 +0100

    chore: bump version number

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExMicrosoftAzureStorage.MixProject do
   use Mix.Project
 
-  @version "1.1.1"
+  @version "1.1.2"
   @repo_url "https://github.com/bettyblocks/ex_microsoft_azure_storage"
 
   def project do
